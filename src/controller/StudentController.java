d60f3bfa31490c685fad594f1559b2f7a2bc1c5e
soyoung-971103 <50395101+soyoung-971103@users.noreply.github.com>
2019-09-09 17:52:07 +0900

    Update StudentController.java
    20190909

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -18,15 +18,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
-import model.DepartDAO;
-import model.DepartDTO;
+import model.LectureDAO;
+import model.LectureDTO;
 import model.StudentDAO;
 import model.StudentDTO;
 //import service.Pagination;
 /**
  * Servlet implementation class StudentController
  */
-@WebServlet({"/student-list.do","/student-studentnew.do","/student-register.do","/student-delete.do","/student-detail.do","/student-update.do"})
+@WebServlet({"/student-list.do","/student-search.do","/student-register.do","/student-delete.do","/student-detail.do","/student-update.do",})
 @MultipartConfig(location="", 
 fileSizeThreshold=1024*1024, 
 maxFileSize=1024*1024*5, 
@@ -47,14 +47,17 @@ public class StudentController extends HttpServlet {
     StudentDTO student = null;
     HttpSession sesobj = null;
     StudentDAO dao = new StudentDAO();
-    ArrayList<DepartDTO> dtoListDepart = null;
-    DepartDAO daoDepart = new DepartDAO();
+    ArrayList<LectureDTO> alLecture = null;
+    LectureDTO lecture = null;
+    LectureDAO lecture_dao = new LectureDAO();
     //Pagination pn = new Pagination();
     
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");		
-		sesobj = request.getSession();	
+		sesobj = request.getSession();
+		
+		System.out.println("process");		
 		
 		String uri = request.getRequestURI();
 		int lastIndex = uri.lastIndexOf('/'); 
@@ -70,34 +73,30 @@ public class StudentController extends HttpServlet {
 			detail(request, response);
 		else if(action.equals("student-update.do")) 
 			update(request, response);
-		else if(action.equals("student-studentnew.do")) 
-			studentnew(request, response);
+		else if(action.equals("student-search.do")) 
+			search(request, response);
     	else 
     		;
 		
 	}
 	
 	protected void list(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
-		 alStudent = dao.list(request.getParameter("text1"));
+		 alStudent = dao.list();
 			request.setAttribute("studentlist", alStudent);
 			request.getRequestDispatcher("ad_student.jsp").forward(request, response);
 	}
 	
-	private void studentnew(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-    	
-    	dtoListDepart = daoDepart.List();
- 
-    	request.setAttribute("listDepart", dtoListDepart);    	   		
-    	request.getRequestDispatcher("ad_studentnew.jsp").forward(request, response);
 	
-	} 
+	protected void search(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException{
+		 alStudent = dao.search(request.getParameter("text1"));
+			request.setAttribute("studentlist", alStudent);
+			request.getRequestDispatcher("ad_student.jsp").forward(request, response);
+	}
 	
 	private void detail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
 		int id = Integer.parseInt(request.getParameter("id"));
+		System.out.println(id);
 		student = dao.detail(id);
-		dtoListDepart = daoDepart.List();
-
-       	request.setAttribute("listDepart", dtoListDepart);
 		request.setAttribute("student", student);
 		String phone[]=student.getPhone().split("-");
 		String birthday[]=student.getBirthday().split("-");
@@ -145,6 +144,7 @@ public class StudentController extends HttpServlet {
 	    }
 	    
     	int result = dao.update(request, response); // 질의를 통해 수정된 레코드의 수
+    	System.out.println("result값: "+result);
     	if(result > 0) {// 성공 가입
     		request.setAttribute("id", request.getParameter("id"));
     		request.getRequestDispatcher("student-list.do").forward(request, response);
@@ -194,6 +194,7 @@ public class StudentController extends HttpServlet {
     	return null;
     }
 
+	
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	try {
 			process(request,response);
